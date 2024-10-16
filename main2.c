601becb2273fdbf5498a485e7d9aa2637c092f20
ackindev <ackinlino123@gmail.com>
2024-10-16 13:58:18 -0300

    Praticando a linguagem C

--- main2.c
+++ main2.c
@@ -5,11 +5,20 @@
 #define pi 3.14;
 
 int main() {
-
+  int n1, n2, total;
+  n1 = 7;
+  n2 = 3;
+  total = n1 - n2;
+  const int idadeDeCachorro = 7;
+  int idade;
   const int p = varconst;
-
+  const float piValor = pi;
   printf("%d", p);
-
+  printf("\nValor do PI = %f", piValor);
+  printf("\nTotal = %d", total);
+  printf("\nDigite a idade do seu cão: ");
+  scanf("%d",&idade);
+  printf("\nIdade do seu cão em idade de cachorro é %d anos.", idadeDeCachorro*idade);
   return 0;
 }
 
